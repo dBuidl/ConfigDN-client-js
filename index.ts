01804eeac73afd91c3f1c29f7c10d641ed395a1e
Rishi Panthee <rrpanthe@ncsu.edu>
2022-11-27 14:35:44 -0500

    v0.0.1

--- index.ts
+++ index.ts
@@ -10,12 +10,22 @@ export class ConfigDN {
     #lastUpdate : number;
     fetchedConfig: Map<String, Object> = new Map();
     
+    /**
+     * Creates a new instance
+     * @param authKey Authorization key, required
+     * @param apiEndpoint Endpoint, defaults to ConfigDN CDN
+     * @param refreshInterval How often to refresh config in seconds, defaults to 60 seconds
+     */
     constructor(authKey: string, apiEndpoint: string = 'https://cdn.configdn.com/', refreshInterval: number = 60) {
         this.#settings = new Settings(authKey, apiEndpoint, refreshInterval);
         this.refreshConfig(true);
         this.#lastUpdate = Date.now() / 1000;
     }
 
+    /**
+     * Forces a refresh of config
+     * @param errorOnFail Throws an error if it fails
+     */
     async refreshConfig(errorOnFail: boolean = false): Promise<void> {
         await axios.request({
             method: 'GET',
@@ -40,6 +50,11 @@ export class ConfigDN {
         })
     }
 
+    /**
+     * Gets the value for a key, if local congif is blank, will attempt to retrieve it first, if it's time to refresh, it will wait for refresh first
+     * @param key Key to get value for
+     * @returns Value
+     */
     async get(key: string): Promise<any> {
         if (this.fetchedConfig.size === 0 || this.#lastUpdate + this.#settings.getRefreshInterval() > Date.now() / 1000) {
             await this.refreshConfig()
@@ -47,6 +62,11 @@ export class ConfigDN {
         return this.getLocal(key);
     }
 
+    /**
+     * Gets value for a key, will refresh in the background for new config it it's time
+     * @param key Key to get value for
+     * @returns Value
+     */
     getLocal(key : string) : any {
         if (this.#lastUpdate + this.#settings.getRefreshInterval() > Date.now() / 1000){
             this.refreshConfig()
@@ -58,6 +78,10 @@ export class ConfigDN {
         }
     }
 
+    /**
+     * Changes refresh interval
+     * @param newInterval new interval
+     */
     changeRefreshInterval(newInterval : number){
         this.#settings.changeRefreshInterval(newInterval);
     }
