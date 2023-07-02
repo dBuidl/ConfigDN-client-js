866ce7bcb2f4b3598aceb55278fa81f25f30b007
Conor <git@dbuidl.com>
2023-07-02 16:06:06 +0100

    Fix library not working with browsers

---
+++ webpack.config.browser.js
@@ -0,0 +1,29 @@
+//webpack.config.browser.js
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    target: ["web", "es5"],
+    devtool: false,
+    entry: {
+        main: "./src/index.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, './dist.browser/src'),
+        filename: "bundle.js" // <--- Will be compiled to this single file
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                options: {
+                    configFile: "tsconfig.browser.json"
+                }
+            }
+        ]
+    }
+};
