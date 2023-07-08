311c0a18c7bd3415974da09f9c37c85343daf4b7
Rishi Panthee <rrpanthe@ncsu.edu>
2023-07-08 10:03:56 -0500

    Release 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ export class ConfigDN {
             method: 'GET',
             url: this.settings.getEndpoint() + 'public_api/v1/get_config/',
             headers: {
-                'ConfigDN-Client-Version': 'ConfigDN-JS/' + version,
+                'User-Agent': 'ConfigDN-JS/' + version,
                 'Authorization': this.settings.getAuthKey(),
                 'Content-Type': 'application/json'
             }
